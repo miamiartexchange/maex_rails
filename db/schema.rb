c5d6591743422afd3a2c7f5d9f4b3f6176be49c4
Paul Kruger <paul@miamiphp.org>
2009-05-28 07:12:55 +0800

    Added museum listing
    Signed-off-by: miamiartexchange <editor@miamiartexchange.com>

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090502195950) do
+ActiveRecord::Schema.define(:version => 20090527230147) do
 
   create_table "artists", :force => true do |t|
     t.string   "first_name"
@@ -33,6 +33,19 @@ ActiveRecord::Schema.define(:version => 20090502195950) do
     t.string   "state"
     t.string   "zip"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "museums", :force => true do |t|
+    t.string   "name"
+    t.string   "address"
+    t.string   "city"
+    t.string   "county"
+    t.string   "state"
+    t.string   "zip"
+    t.string   "phone"
+    t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
